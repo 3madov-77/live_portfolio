bd86db276dff3ea3da61433ea90d2854e0ac1ad4
3madov-77 <emadzxy7@gmail.com>
2021-03-25 19:23:17 +0200

    test

---
+++ server.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const express = require('express');
+
+const server = express();
+
+const PORT = process.env.PORT || 4444;
+server.use(express.static('./public'))
+
+
+
+server.listen(PORT, ()=>{
+    console.log('your Port is:', PORT);
+})
+
+server.get('/data', (request, response) =>{
+    response.send('Heloo World...')
+
+})
